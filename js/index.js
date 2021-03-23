bd2948d72f798b14ef1a58a41b6929273c2d68c2
bt-kc <72064957+bt-kc@users.noreply.github.com>
2021-03-23 19:44:31 +0700

    fixed some javascripts (navbar,form,...) & edited some "About" page components

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-//button unclickable
+
 let username_input = document.getElementById("username");
 let btn_start = document.getElementById("btn-start");
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (!(btn_start.value == null || this.value=='')){
+        btn_start.classList.remove("disabled");
+    }
+});
+
+//button unclickable
 username_input.addEventListener("change",function(){
     if (this.value == null || this.value==''){
         btn_start.classList.add("disabled");
